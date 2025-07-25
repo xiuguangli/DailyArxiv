1625bb9c6c1da7c16779c1a24d367111fdafb4a1
lxg <li.xgxv@gmail.com>
2025-07-25 09:57:38 +0800

    更新代码和部署

--- get_data/get_arxiv_paper/run_all.sh
+++ get_data/get_arxiv_paper/run_all.sh
@@ -7,7 +7,6 @@ export TZ=Asia/Shanghai
 # 获取当天日期
 DATE=$(date -d '8 hours' +%Y-%m-%d)
 BASE_DIR="public/arxiv"
-DATE=2025-07-21
 
 # 运行 get_arxiv.py
 python get_data/get_arxiv_paper/get_arxiv.py --date "$DATE" --base_dir "$BASE_DIR"
